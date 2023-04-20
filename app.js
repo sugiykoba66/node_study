a8bbf34725d2db43766e02be2b37e35f39917075
sugiykoba66 <y-kobayashi@sugi-pharmacy.co.jp>
2023-04-20 17:26:57 +0900

    second commit

--- app.js
+++ app.js
@@ -2,15 +2,28 @@ var http = require('http');
 var path = require('path');
 var morgan = require('morgan');
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
 app.use(morgan("combined"));
 
+app.use("/static",express.static(path.join(__dirname,"static")));
+
+app.use(bodyParser.urlencoded({extended:false}));
+
 app.set('views',path.join(__dirname,'templetes'));
-app.set('view engine','pug');
+app.set('view engine','ejs');
 
 app.get("/",function(req,res){
     return res.render("index",{title:"Hello World"});
+});
+
+app.get("/form",function(req,res){
+    return res.render("form");
+});
+
+app.post("/form",function(req,res){
+    return res.render("result",{username:req.body.username,message:req.body.message});
 });
 
 var server = http.createServer(app);
